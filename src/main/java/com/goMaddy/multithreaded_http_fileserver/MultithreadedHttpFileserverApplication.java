d985dd6720ca5d83268ebd9fe9be53a186141f22
go8089 <gopaljha8409@gmail.com>
2026-07-07 09:50:13 +0000

    vi

---
+++ src/main/java/com/goMaddy/multithreaded_http_fileserver/MultithreadedHttpFileserverApplication.java
@@ -0,0 +1,13 @@
+package com.goMaddy.multithreaded_http_fileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadedHttpFileserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadedHttpFileserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goMaddy/multithreaded_http_fileserver/MultithreadedHttpFileserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goMaddy.multithreaded_http_fileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadedHttpFileserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
